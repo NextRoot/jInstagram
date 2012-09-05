7946c3bb6ea5b079f5eb782d5049cf5180358bce
Stefan Filip <stefan@nextroot.com>
2012-09-05 22:25:52 +0300

    Changed the id field to be of type long instead of String

--- jInstagram/src/java/main/org/jinstagram/entity/users/basicinfo/UserInfoLogin.java
+++ jInstagram/src/java/main/org/jinstagram/entity/users/basicinfo/UserInfoLogin.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class UserInfoLogin {
 	@SerializedName("id")
-	private String id;
+	private long id;
 	
 	@SerializedName("username")
 	private String username;
@@ -15,11 +15,11 @@ public class UserInfoLogin {
 	@SerializedName("profile_picture")
 	private String profilePicture;
 	
-	public String getId() {
+	public long getId() {
 		return id;
 	}
 	
-	public void setId(String id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 	
